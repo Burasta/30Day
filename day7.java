4884c4d47faba6cfd38b4a2cfcb3e3499ec75ec6
Brandon Camp <brandon-camp@georgiasouthern.edu>
2016-06-20 15:55:04 -0400

    Added main()

--- day7.java
+++ day7.java
@@ -1,8 +1,23 @@
+// Score: 30/30
+
 import java.util.Scanner;
 
 public class day7 {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        input.useDelimiter("\n");
+        int cases = input.nextInt();
+        String[] numbers = input.next().split(" ");
+        input.close();
+
+        for (int i = (numbers.length - 1); i > 0; i--) {
+            System.out.print(numbers[i]);
+            System.out.print(" ");
+        }
+
+        System.out.println(numbers[0]);
 
     }
 }
