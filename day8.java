2441b437a34eda312779e6a0f394632472b193c2
Brandon Camp <brandon-camp@georgiasouthern.edu>
2016-06-22 13:39:08 -0400

    day8

---
+++ day8.java
@@ -0,0 +1,43 @@
+// Score: 30/30
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class day8 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int entries = input.nextInt();
+        String first = "";
+        String second = "";
+
+        Map<String, String> phoneBook = new HashMap<>();
+
+        for(int i = 0; i < entries; i++) {
+            phoneBook.put(input.next(), input.next());
+        }
+
+        ArrayList searches = new ArrayList();
+
+        while(input.hasNext()) {
+            String name = input.next();
+            if(name.equals("0")) {
+                break;
+            }
+            searches.add(name);
+        }
+
+        input.close();
+
+        for (Object search : searches) {
+            String name = (String) search;
+            if (phoneBook.get(name) == null) {
+                System.out.println("Not found");
+            } else {
+                System.out.println(name + "=" + phoneBook.get(name));
+            }
+        }
+    }
+}
