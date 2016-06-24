2ba935ce44e5292a82b007d3ac07c6857c0793b1
Brandon Camp <brandon-camp@georgiasouthern.edu>
2016-06-24 12:10:44 -0400

    day11

---
+++ day11.java
@@ -0,0 +1,42 @@
+import java.lang.Integer;
+import java.util.Scanner;
+
+public class day11 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        input.useDelimiter("\n");
+
+        String s = input.next();
+        String[] first = s.split(" ");
+        int length = first.length;
+        int[][] matrix = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+            matrix[0][i] = Integer.parseInt(first[i]);
+        }
+
+        for (int i = 1; i < length; i++) {
+            String[] line = input.next().split(" ");
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        input.close();
+
+        int max = 0;
+
+        for (int i = 0; i < length - 2; i++) {
+            for (int j = 0; j < length - 2; j++) {
+                int sum = matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] +              // First row of the I-hourglass.
+                          matrix[i + 1][j + 1] +                                            // Center row of the I-hourglass.
+                          matrix[i + 2][j] + matrix[i + 2][j + 1] + matrix[i + 2][j + 2];   // Last row of the I-hourglass.
+                max = (max < sum) ? sum : max;
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
